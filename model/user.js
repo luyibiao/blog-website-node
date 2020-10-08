17cc31ae4907947e3152937fd11823c435f81634
luyibiao <lu.yibiao@hb-cloud.cn>
2020-10-08 14:55:13 +0800

    完善系统架构

--- model/user.js
+++ model/user.js
@@ -1,10 +1,22 @@
 var userSql = require('../sql/user.js');
 var db = require('../config/index.js');
-const helpers = require('../untils/helper')
+var token = require('../token/token')
 
-function getUser(callback, req) {
-  db.queryArgs(userSql.loginSql, [helpers.getValue(req, 'account'), helpers.getValue(req, 'password')], function(result) {
-    callback(result)
+function getUser(req, res) {
+  db.queryArgs(userSql.loginSql, global.$getArgs(req, 'account', 'password'), function(err, result) {
+    if (err) {
+      global.$resultFn.resultErr(err)
+    } else {
+      let r = global.$resultFn.resultSuccess(result)
+      if (!r.data.id) {
+        r = global.$resultFn.resultNouser()
+      } else {
+        r.data.token = token.createToken({
+          id: r.data.id
+        })
+      }
+      res.json(r)
+    }
   })
 }
 
--- untils/code.js
+++ untils/code.js
@@ -1,10 +1,13 @@
+
 const codes = {
   // 成功状态
   success: '1',
   // 失败状态
   err: '00',
   // token验证失败
-  tokenerr: '-1'
+  tokenerr: '-1',
+  // 未查找到用户
+  nouser: '-2'
 }
 
 // 成功返回字段
@@ -12,7 +15,7 @@ function success(res) {
   return {
     code: codes.success,
     msg: 'success',
-    data: res
+    data: global.$formatRes(res)
   }
 }
 
@@ -25,15 +28,41 @@ function error(err, code) {
 }
 
 // token验证失败返回字段
-function TokenErr(res) {
+function TokenErr() {
   return {
     msg: 'token验证失败，请重新登录',
-    code: code.tokenerr
+    code: codes.tokenerr
+  }
+}
+
+// 未找到用户
+function nouser() {
+  return {
+    msg: '未找到用户',
+    code: codes.nouser
+  }
+}
+
+const results = {
+  // 成功
+  resultSuccess: function(res) {
+    return success(res)
+  },
+  // 失败
+  resultErr: function(err, code) {
+    return error(err, code)
+  },
+  // token验证失败
+  resultTokenErr: function() {
+    return TokenErr()
+  },
+  // 未找到用户
+  resultNouser: function() {
+    return nouser()
   }
 }
 
 module.exports = {
-  success,
-  error,
-  TokenErr
+  results,
+  codes
 }
