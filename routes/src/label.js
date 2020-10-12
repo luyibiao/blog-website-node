918d877b05f0e1cf2142cf1e79a908483f46231d
luyibiao <lu.yibiao@hb-cloud.cn>
2020-10-12 18:16:48 +0800

    增加标签增删改成功能

--- routes/src/label.js
+++ routes/src/label.js
@@ -5,8 +5,16 @@ global.$router.post('/addLabel', function(req, res, next) {
   api.addLabel(req, res, next)
 })
 
+global.$router.post('/update', function(req, res, next) {
+  api.updateLabel(req, res, next)
+})
+
 global.$router.post('/list', function(req, res, next) {
   api.queryLabel(req, res, next)
+})
+
+global.$router.post('/delete', function(req, res, next) {
+  api.deleteLabel(req, res, next)
 })
 
 
--- sql/label.js
+++ sql/label.js
@@ -1,36 +1,14 @@
 
-
-const mysql = require('mysql')
-
-// 拼接条件查询语句sql语句
-function relyOn(key, req, ispage = true) {
-  key = key || []
-  const params = req.body
-  // 提取
-  let str = ``
-  let i = false
-  key.filter(v => v !=='pageIndex' && v !== 'pageSize' ).map((v, index) => {
-    if (params[v]) {
-      i = true
-      str += `${index === 0 && 'where '}${index > 0 ? 'and ' : ''}${v} = ${mysql.escape(params[v])}`
-    }
-  })
-
-  // 开启了分页， 传参需要在最后
-  if (ispage) {
-    str += ' limit ?, ?'
-    // str += ` ${i ? 'and' : 'where '} id > ? limit ?`
-  }
-  return str
-}
-
 const sql = {
   addLabelSql: 'insert into label set label=?',
   queryAll: function(key, req) {
-    const s1 = `select * from label ` + relyOn(key, req)
-    const s2 = `;select count(*) from label ` + relyOn(key, req, false)
+    const s1 = `select * from label ` + global.$overall.relyOn(key, req) + 'ORDER BY create_time desc limit ?, ?'
+    const s2 = `;select count(*) from label ` + global.$overall.relyOn(key, req, false)
     return s1 + s2 
-  }
+  },
+  // 修改 
+  update: 'update label set label = ? where id = ?',
+  delete: 'delete from label where id = ?'
 }
 
 module.exports = sql
