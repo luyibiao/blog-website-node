5480f1b47cb40b26e157b278541b8fbfe98c40de
luyibiao <lu.yibiao@hb-cloud.cn>
2020-10-14 18:21:56 +0800

    优化图片上传

--- routes/src/upload.js
+++ routes/src/upload.js
@@ -1,21 +1,45 @@
 
 const formidable = require('formidable');
 const path = require('path')
+const fs = require('fs')
 
 global.$router.post('/upload', function(req, res, next) {
-  let form = new formidable.IncomingForm()
+    let form = new formidable.IncomingForm()
     form.encoding = 'utf-8' // 编码
     form.keepExtensions = true // 保留扩展名
-    form.uploadDir = path.join(__dirname, '../../images/')
+    // 临时目录
+    form.uploadDir = path.join(__dirname, '../../images/temporary/')
     form.parse(req, (err, fields, files) => {
-      if (err) return next(err)
-      console.log(fields) //Object 表单数据
-      console.log(files) //上传文件用files.<name>访问
-      res.json({
-        code: 1,
-        message: 'upload success',
-        data: files
-      })
+      if (err) {
+        res.json(global.$resultFn.resultErr(err))
+        return
+      }
+      for(let key in files) {
+        let file = files[key]
+        let fName = (new Date()).getTime();
+        switch (file.type){
+          case "image/jpeg":
+              fName = fName + ".jpg";
+              break;
+          case "image/png":
+              fName = fName + ".png";
+              break;
+          default :
+              fName =fName + ".png";
+              break;
+        }
+        var uploadDir = path.join(__dirname, '../../images/real/' + fName) ;
+        fs.rename(file.path, uploadDir, function(err) {
+          if (err) {
+              res.write(err+"\n");
+              res.end();
+          }
+          console.log(path.join(__dirname, '../../images/real/'))
+          res.json(global.$resultFn.resultSuccess({fName: path.join(__dirname, '../../images/real/' ) + fName}))
+          res.end();
+        });
+      }
+      
     })
 })
 
