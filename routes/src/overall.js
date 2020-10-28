9f79d085a1acf210b53ee1107262dcce639d1d44
luyibiao <1739717921@qq.com>
2020-10-28 18:06:17 +0800

    增加热评音乐api

--- routes/src/overall.js
+++ routes/src/overall.js
@@ -6,5 +6,25 @@ global.$router.post('/queryArticleType', function(req, res, next) {
   model.queryArticleType(req, res, next)
 })
 
+// 转发网易云热评歌曲api 
+global.$router.post('/queryWYYmusic', function(req, res, next ) {
+  var request = require('request');
+  let list = []
+  var url = 'https://tenapi.cn/comment/'
+  for(let i = 0; i < 10; i++) {
+    request(url, function (err, response, body) {
+      var data = JSON.parse(body);
+      list.push(data)
+      if (i === 9) {
+        if (!err && response.statusCode === 200) {
+          res.send(global.$resultFn.resultSuccess(list, false));
+        } else {
+          res.json(global.$resultFn.resultErr(err))
+        }
+      }
+    })
+  }
+})
+
 
 module.exports = global.$router
