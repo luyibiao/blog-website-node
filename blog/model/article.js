4daceb0d4e27834830ea280a0d798359bbb3ef9f
luyibiao <1739717921@qq.com>
2020-11-02 20:04:56 +0800

    增加邮件发送功能

--- blog/model/article.js
+++ blog/model/article.js
@@ -2,6 +2,42 @@ const {article, label: labSql, banner: bannerSql } = global.$sql('article', 'lab
 
 // 拿文章列表
 function getArticleList(req, res) {
+  var nodemailer = require('nodemailer');
+  // 创建一个SMTP客户端配置
+  var config = {
+    host: 'smtp.qq.com',//网易163邮箱 smtp.163.com
+    port: 465,//网易邮箱端口 25
+    auth: {
+        user: '1739717921@qq.com', //邮箱账号
+        pass: 'hpkzbnopseccceij'  //邮箱的授权码
+    }
+  };
+
+  // 创建一个SMTP客户端对象
+  var transporter = nodemailer.createTransport(config);
+
+  // 发送邮件
+function send(mail){
+  transporter.sendMail(mail, function(error, info){
+      if(error) {
+          return console.log(error);
+      }
+      console.log('mail sent:', info.response);
+  });
+};
+
+// 创建一个邮件对象
+var mail = {
+  // 发件人
+  from: 'lu<1739717921@qq.com>',
+  // 主题
+  subject: '狗蒋',
+  // 收件人
+  to: '1739717921@qq.com',
+  // 邮件内容，HTML格式
+  // text: '点击激活：xxx' //可以是链接，也可以是验证码
+};
+send(mail);
   const pagesList = global.$overall.setPagination(req)
   const arrs = [
     'title', 'create_time', 'type', 'author', 'status', 'child_type', 'hot_comments', 'topping'
