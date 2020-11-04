361cbec8897f410bfe9fb204a5df3f252d6e0209
luyibiao <1739717921@qq.com>
2020-11-04 18:12:07 +0800

    修改文章接口

--- blog/routes/src/article.js
+++ blog/routes/src/article.js
@@ -6,7 +6,7 @@ global.$blogRouter.post('/getArticleList', (req, res, next) => {
 
 // 查询文章详情
 global.$blogRouter.post('/queryArticleDetail', (req, res, next) => {
-  article.getArticleList(req, res, next)
+  article.queryArticleDetail(req, res, next)
 })
 
 module.exports = global.$blogRouter
